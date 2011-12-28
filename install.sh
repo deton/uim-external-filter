3029416d8dee460bd46ca6d8778bb2a39ccc68b9
KIHARA Hideto <deton@m1.interq.or.jp>
2011-12-28 17:57:21 +0900

    Change to use $srcdir to install from another directory.

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+srcdir=$(dirname $0)
 scmdir=$(pkg-config --variable=uim_scmdir uim)
 pixmapsdir=$(pkg-config --variable=uim_datadir uim)/pixmaps
-cp external-filter.scm external-filter-custom.scm "$scmdir"
-cp pixmaps/external-filter.png pixmaps/external-filter_dark_background.png "$pixmapsdir"
-cp pixmaps/external-filter.svg pixmaps/external-filter_dark_background.svg "$pixmapsdir"
+cp "$srcdir/external-filter.scm" "$srcdir/external-filter-custom.scm" "$scmdir"
+cp "$srcdir/pixmaps/external-filter.png" "$srcdir/pixmaps/external-filter_dark_background.png" "$pixmapsdir"
+cp "$srcdir/pixmaps/external-filter.svg" "$srcdir/pixmaps/external-filter_dark_background.svg" "$pixmapsdir"
 uim-module-manager --register external-filter
